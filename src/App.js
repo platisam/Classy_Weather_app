dd2ca930dc9cb0a00b03af56a4dfb4b7b825a272
platisam <platisam@gmail.com>
2023-07-02 12:22:19 +0200

    creating class component - Counter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div className="App">Weather App</div>;
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { count: 5 };
+  }
+
+  render() {
+    return (
+      <div>
+        <button>-</button>
+        <span>{this.state.count}</span>
+        <button>+</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Counter;
