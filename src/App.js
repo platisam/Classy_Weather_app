f51a28bbc70ee2ba0e31984cfc51e48672e2516b
platisam <platisam@gmail.com>
2023-07-02 13:32:08 +0200

    building basic JSX - weather app

--- src/App.js
+++ src/App.js
@@ -1,41 +1,18 @@
+import { render } from "@testing-library/react";
 import React from "react";
 
-class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { count: 5 };
-    this.handleDecrement = this.handleDecrement.bind(this);
-    this.handleIncrement = this.handleIncrement.bind(this);
-  }
-
-  handleDecrement() {
-    //console.log(this);
-    this.setState((curState) => {
-      return { count: curState.count - 1 };
-    });
-  }
-
-  handleIncrement() {
-    //console.log(this);
-    this.setState((curState) => {
-      return { count: curState.count + 1 };
-    });
-  }
-
+class App extends React.Component {
   render() {
-    const date = new Date("june 21 2027");
-    date.setDate(date.getDate() + this.state.count);
     return (
-      <div>
-        <button onClick={this.handleDecrement}>-</button>
-        <span>
-          {date.toDateString()}[{this.state.count}]
-        </span>
-        <button onClick={this.handleIncrement}>+</button>
+      <div className="app">
+        <h1>Classy Weather</h1>
+        <div>
+          <input type="text" placeholder="Search from location..." />
+        </div>
+        <button>Get weather</button>
       </div>
     );
   }
 }
 
-export default Counter;
+export default App;
