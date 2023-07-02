4ac96a37d05e2a3418ff6c354796dab8a1a6dbca
platisam <platisam@gmail.com>
2023-07-02 17:07:27 +0200

    added component lifecycle methods componentDidMount and componentDidUpdate

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function formatDay(dateStr) {
 
 class App extends React.Component {
   state = {
-    location: "lisbon",
+    location: "",
     isLoading: false,
     displayLocation: "",
     weather: {},
@@ -47,6 +47,8 @@ class App extends React.Component {
 
   // async fetchWeather() {
   fetchWeather = async () => {
+    if (this.state.location.length < 2) return;
+
     try {
       this.setState({ isLoading: true });
       // 1) Getting location (geocoding)
@@ -70,13 +72,25 @@ class App extends React.Component {
       const weatherData = await weatherRes.json();
       this.setState({ weather: weatherData.daily });
     } catch (err) {
-      console.err(err);
+      console.log(err);
     } finally {
       this.setState({ isLoading: false });
     }
   };
 
   setLocation = (e) => this.setState({ location: e.target.value });
+
+  // useEffect []
+  componentDidMount() {
+    //this.fetchWeather();
+  }
+
+  // useEffect [location]
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+    }
+  }
 
   render() {
     return (
@@ -86,7 +100,6 @@ class App extends React.Component {
           location={this.state.location}
           onChangeLocation={this.setLocation}
         />
-        <button onClick={this.fetchWeather}>Get weather</button>
 
         {this.state.isLoading && <p className="loader">Loading...</p>}
 
